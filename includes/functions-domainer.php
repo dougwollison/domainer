86658ba293cf70a56fbf858928e066eaad312f48
Doug Wollison <doug@wollison.net>
2017-04-26 14:41:13 -0400

    Add WP_INSTALLING to is_backend(), and use SCRIPT_NAME instead of $pagenow

--- includes/functions-domainer.php
+++ includes/functions-domainer.php
@@ -26,13 +26,18 @@ namespace Domainer;
  * @global string $pagenow The current page slug.
  */
 function is_backend() {
+	if ( defined( 'WP_INSTALLING' ) && WP_INSTALLING ) {
+		// "Install" process, count as backend
+		return true;
+	}
+
 	if ( defined( 'DOING_AJAX' ) && DOING_AJAX ) {
 		// AJAX request, check if the referrer is from wp-admin
 		return strpos( $_SERVER['HTTP_REFERER'], admin_url() ) === 0;
-	} else {
-		// Check if in the admin or otherwise the login/register page
-		return is_admin() || in_array( basename( $_SERVER['SCRIPT_NAME'] ), array( 'wp-login.php', 'wp-register.php' ) );
 	}
+
+	// Check if in the admin or otherwise the login/register page
+	return is_admin() || in_array( $_SERVER['SCRIPT_NAME'], array( 'wp-login.php', 'wp-register.php' ) );
 }
 
 // =========================
