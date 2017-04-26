24faa3e42e2dd43af88e607ae9f7245b53217d4f
Doug Wollison <doug@wollison.net>
2017-04-26 14:35:40 -0400

    Use SCIRPT_NAME instead of $pagenow; won't be defined yet

--- includes/functions-domainer.php
+++ includes/functions-domainer.php
@@ -26,14 +26,12 @@ namespace Domainer;
  * @global string $pagenow The current page slug.
  */
 function is_backend() {
-	global $pagenow;
-
 	if ( defined( 'DOING_AJAX' ) && DOING_AJAX ) {
 		// AJAX request, check if the referrer is from wp-admin
 		return strpos( $_SERVER['HTTP_REFERER'], admin_url() ) === 0;
 	} else {
 		// Check if in the admin or otherwise the login/register page
-		return is_admin() || in_array( $pagenow, array( 'wp-login.php', 'wp-register.php' ) );
+		return is_admin() || in_array( basename( $_SERVER['SCRIPT_NAME'] ), array( 'wp-login.php', 'wp-register.php' ) );
 	}
 }
 
