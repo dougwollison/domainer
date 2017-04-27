6e032afa3fc47201e64fad1c6091fed287c3298a
Doug Wollison <doug@wollison.net>
2017-04-27 17:39:40 -0400

    Add rewrite_url helper

--- includes/functions-template.php
+++ includes/functions-template.php
@@ -10,6 +10,32 @@
  * @since 1.0.0
  */
 
-use Domainer\Registry as Registry;
+/**
+ * Filter the content to replace the domain name.
+ *
+ * @since 1.0.0
+ *
+ * @param string       $content    The content to filter.
+ * @param string|array $old_domain Optional. The original domain(s) to replace
+ *                                 (defaults to blog's original).
+ * @param string       $new_domain Optional. The new domain to replace with
+ *                                 (defaults to blog's primary).
+ */
+function domainer_rewrite_url( $content, $old_domain = null, $new_domain = null ) {
+	if ( is_null( $old_domain ) ) {
+		$old_domain = Domainer\get_true_url();
+	}
 
-// to be written
+	if ( is_null( $new_domain ) ) {
+		global $current_blog;
+		$new_domain = rtrim( $current_blog->domain . $current_blog->path, '/' );
+
+		if ( $domain = Registry::get_domain( $current_blog->domain_id ) ) {
+			$new_domain = $domain->fullname();
+		}
+	}
+
+	$content = str_replace( (array) $old_domain, $new_domain, $content );
+
+	return $content;
+}
