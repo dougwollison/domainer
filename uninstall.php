7e102ae3c77bcc7bc0aa38d788c9d979aece6ec0
Doug Wollison <doug@wollison.net>
2017-04-28 13:07:34 -0400

    Simplify uninstall script to a function, not a class

--- uninstall.php
+++ uninstall.php
@@ -10,7 +10,7 @@
 namespace Domainer;
 
 /**
- * The Plugin Uninstaller
+ * Uninstall Domainer.
  *
  * Handles removal of tables and options created by the plugin.
  *
@@ -18,61 +18,35 @@ namespace Domainer;
  *
  * @since 1.0.0
  */
-final class Uninstaller {
-	/**
-	 * Run the uninstallation.
-	 *
-	 * Will check for Multisite and run uninstall() for each blog.
-	 *
-	 * @since 1.0.0
-	 */
-	public static function run() {
-		// Abort if not running in the WordPress context.
-		if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
-			die( 'Must be run within WordPress context.' );
-		}
+public static function uninstall() {
+	global $wpdb;
 
-		// Also abort if (somehow) it's some other plugin being uninstalled
-		if ( WP_UNINSTALL_PLUGIN != basename( __DIR__ ) . '/domainer.php' ) {
-			die( sprintf( 'Illegal attempt to uninstall [Plugin Name] while uninstalling %s.', WP_UNINSTALL_PLUGIN ) );
-		}
-
-		// Check if this site is a Multisite installation
-		if ( is_multisite() ) {
-			// Run through each blog and perform the uninstall on each one
-			$sites = get_sites( array(
-				'fields' => 'ids',
-			) );
-
-			foreach ( $sites as $site ) {
-				switch_to_blog( $site );
-
-				self::uninstall();
-
-				restore_current_blog();
-			}
-		} else {
-			self::uninstall();
-		}
+	// Abort if not running in the WordPress context.
+	if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+		die( 'Must be run within WordPress context.' );
 	}
 
-	/**
-	 * Perform the actual uninstallation.
-	 *
-	 * Delete all tables and all options created by Domainer.
-	 *
-	 * @since 1.0.0
-	 *
-	 * @global \wpdb $wpdb The database abstraction class instance.
-	 */
-	public static function uninstall() {
-		global $wpdb;
-
-		// Delete the object and string translation tables
-		$wpdb->query( "DROP TABLE IF EXISTS {$wpdb->prefix}domainer" );
-
-		delete_option( 'domainer_options' );
+	// Also abort if (somehow) it's some other plugin being uninstalled
+	if ( WP_UNINSTALL_PLUGIN != basename( __DIR__ ) . '/domainer.php' ) {
+		die( sprintf( 'Illegal attempt to uninstall [Plugin Name] while uninstalling %s.', WP_UNINSTALL_PLUGIN ) );
 	}
+
+	// Run through each blog and perform the uninstall on each one
+	$sites = get_sites( array(
+		'fields' => 'ids',
+	) );
+
+	foreach ( $sites as $site ) {
+		delete_blog_option( $site, 'domainer_option_overrides' );
+	}
+
+	delete_site_option( 'domainer_options' );
+
+	// Delete the object and string translation tables
+	$wpdb->query( "DROP TABLE IF EXISTS {$wpdb->prefix}domainer" );
+
+	delete_site_option( 'domainer_database_version' );
+}
 }
 
-Uninstaller::run();
+uninstall();
